43f20ac3f812b9d05018ae674f78dd1758c38e67
Ravi1193 <56916048+Ravi1193@users.noreply.github.com>
2021-10-05 10:17:40 +0530

    initialization

---
+++ Service/ProjectInstaller.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        public ProjectInstaller()
+        {
+            InitializeComponent();
+            /*If we don't add this line it will asked for Service login id and password*/
+            this.serviceProcessInstaller1.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
+            /*This line for rename the service name*/
+            this.serviceInstaller1.ServiceName = "EmailService";
+        }
+    }
+}
